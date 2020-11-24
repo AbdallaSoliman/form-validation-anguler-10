c7c29b1ef5a2973c969a9976e600f1b843e20cdd
AbdallaSoliman <36964906+AbdallaSoliman@users.noreply.github.com>
2020-11-24 02:55:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
 app.use(express.static('form-validation-anguler-10'));
+app.get('/*', function(req, res) {
+  res.sendFile(’index.html’, {root: 'form-validation-anguler-10'}
+);
+});
+app.listen(process.env.PORT || 8080);
